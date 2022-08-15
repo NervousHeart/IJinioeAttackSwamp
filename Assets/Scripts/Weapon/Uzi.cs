d7540dff1ab2c3e441abb36b97138193b133d1db
Dmitrii Kudryashov <jieviaf@gmail.com>
2022-08-15 14:51:42 +0300

    Init

---
+++ Assets/Scripts/Weapon/Uzi.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uzi : Weapon
+{
+    [SerializeField] private int _countBullet;
+    public override void Shoot(Transform shootPoint)
+    {
+        for (int i = 0; i < _countBullet; i++)
+        {
+            Instantiate(Bullet, shootPoint.position + new Vector3(shootPoint.position.x + i, 0), Quaternion.identity);
+        }
+    }
+}
